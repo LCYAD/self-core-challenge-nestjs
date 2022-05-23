02f0553493967ca59a3e64284286a2033e59a215
adrian.lee <alee.lcagency@gmail.com>
2022-05-23 21:37:00 +0800

    feat: setup swagger module

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import {
   FastifyAdapter,
   NestFastifyApplication
 } from '@nestjs/platform-fastify'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 import { AppModule } from './app.module'
 
 async function bootstrap() {
@@ -10,6 +11,17 @@ async function bootstrap() {
     AppModule,
     new FastifyAdapter()
   )
+  if (process.env.APP_ENV !== 'production') {
+    const options = new DocumentBuilder()
+      .setTitle('NestJS Self Challenge API API')
+      .setDescription('An API to get quotation information')
+      .setVersion('1.0')
+      .addBearerAuth()
+      .build()
+    const document = SwaggerModule.createDocument(app, options)
+    SwaggerModule.setup('api', app, document)
+  }
+
   await app.listen(3000)
 }
 bootstrap()
