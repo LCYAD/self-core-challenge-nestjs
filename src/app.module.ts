19bd034bebcc39aacdce33ed158a00f801fa9da5
Adrian Lee <alee.lcagency@gmail.com>
2022-05-24 23:57:50 +0800

    feat: nestjs config setup (#6)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
+import secret from './config/secret'
 import { QuotationsModule } from './modules/quotations/quotations.module'
 
 @Module({
-  imports: [QuotationsModule],
+  imports: [
+    ConfigModule.forRoot({
+      load: [secret]
+    }),
+    QuotationsModule
+  ],
   controllers: [],
   providers: []
 })
