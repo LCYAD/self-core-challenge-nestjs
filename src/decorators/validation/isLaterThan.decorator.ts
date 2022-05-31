ac0a9c1abdbe9aff713b0c0c0b89c4f0dfa57e16
adrian.lee <alee.lcagency@gmail.com>
2022-05-31 15:44:25 +0800

    refactor: is later than decorator to compare multiple properties

--- src/decorators/validation/isLaterThan.decorator.ts
+++ src/decorators/validation/isLaterThan.decorator.ts
@@ -1,8 +1,9 @@
 import type { ValidationArguments, ValidationOptions } from 'class-validator'
 import { registerDecorator } from 'class-validator'
+import _ from 'lodash'
 
-export const IsLaterThan = (
-  property: string,
+export const IsLaterThan = <T>(
+  properties: { key: T | 'now'; laterInSecs?: number }[],
   validationOptions?: ValidationOptions
 ) => {
   return (object, propertyName: string) => {
@@ -10,17 +11,33 @@ export const IsLaterThan = (
       name: 'IsLaterThan',
       target: object.constructor,
       propertyName,
-      constraints: [property],
+      constraints: _.map(properties, 'key'),
       options: validationOptions,
       validator: {
         validate: (value: string, args: ValidationArguments) => {
-          const [relatedPropertyName] = args.constraints
-          const relatedValue: string = (args.object as any)[relatedPropertyName]
-          return new Date(value).getTime() > new Date(relatedValue).getTime()
+          const relatedPropertyNames = args.constraints
+          const propertiesObjByKey = _.keyBy(properties, 'key')
+          return _.every(relatedPropertyNames, (name) => {
+            const relatedValue: string =
+              name === 'now' ? Date.now() : (args.object as any)[name]
+            const laterInSecs: number = _.get(
+              propertiesObjByKey,
+              'key.laterInSecs',
+              0
+            )
+            return (
+              relatedValue &&
+              new Date(value).getTime() >
+                new Date(relatedValue).getTime() + laterInSecs
+            )
+          })
         },
         defaultMessage: (args: ValidationArguments) => {
-          const [relatedPropertyName] = args.constraints
-          return `${propertyName} must be later than ${relatedPropertyName}`
+          const relatedPropertyNames = args.constraints
+          return `${propertyName} must be later than ${_.join(
+            relatedPropertyNames,
+            ', '
+          )}`
         }
       }
     })
